ae973f25381d349ce6d08d3a98e0380cca7f1d7c
Manuel De Leon <manuel.de.leon@accenture.com>
2019-07-14 20:51:35 -0600

    Add tests for message service

--- src/app/message.service.spec.ts
+++ src/app/message.service.spec.ts
@@ -1,15 +1,28 @@
-import { TestBed, inject } from '@angular/core/testing';
-
-import { MessageService } from './message.service';
-
-describe('MessageService', () => {
+import { TestBed } from "@angular/core/testing";
+import { MessageService } from "./message.service";
+describe("MessageService", () => {
+  let service: MessageService;
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [MessageService]
-    });
+    TestBed.configureTestingModule({ providers: [MessageService] });
+    service = TestBed.get(MessageService);
   });
 
-  it('should be created', inject([MessageService], (service: MessageService) => {
+  it("can load instance", () => {
     expect(service).toBeTruthy();
-  }));
+  });
+
+  it("messages defaults to: []", () => {
+    expect(service.messages).toEqual([]);
+  });
+
+  it("adds a message", () => {
+    service.add("ABC");
+    expect(service.messages).toEqual(["ABC"]);
+  });
+
+  it("clears all messages", () => {
+    service.add("ABC");
+    service.clear();
+    expect(service.messages).toEqual([]);
+  });
 });
